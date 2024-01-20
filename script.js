21555d58dfec1c5e5e2b8832ffe79af62d56b9e3
Ivan Rogov <drummerrog86@gmail.com>
2024-01-20 16:01:42 +0300

    домашнее задание 1

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-console.log("Hello");
-const a = 1
+const title = 1
+const screen = 2
+const screenPrice = 3
+const rollback = 4
+const fullPrice = 5
+const adaptive = 6
+
+alert("Achtung!");
+
+console.log("Achtung!!");
