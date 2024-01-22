aef1780859015e7492bd2155194bd918656b8aa3
Ivan Rogov <drummerrog86@gmail.com>
2024-01-22 16:39:50 +0300

    выполнил задание 2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-console.log(1);
+const title = "Project";
+const screen = "Simple, Difficult, Interactive";
+const screenPrice = 11;
+const rollback = 21
+const fullPrice = 101000
+const adaptive = true;
 
-console.log(2);
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screen.length);
+console.log(screenPrice);
+console.log(fullPrice);
+console.log(screen.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
