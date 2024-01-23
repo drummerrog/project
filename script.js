7b5aefd032840bc2fd630b730e60d730fd70f0e8
Ivan Rogov <drummerrog86@gmail.com>
2024-01-23 19:42:26 +0300

    коммит дополненного задания 3

--- script.js
+++ script.js
@@ -3,46 +3,43 @@
 alert('Как называется ваш проект?');
 
 let title = prompt("Как называется ваш проект?");
-console.log(title);
-
 let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Cложные, Интерактивные");
-console.log(screens);
-
 let screenPrice = +prompt("Сколько будет стоить данная работа?", 24000);
-
 let rollback = 21;
-
 let adaptive = prompt("Нужен ли адаптив на сайте?");
-
-if (adaptive == "Да") {
-  console.log(true);
-} else if (adaptive == "да") {
-  console.log(true);
-} else {
-  console.log(false);
-}
-
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
 let servicePercentPrice = fullPrice - fullPrice * rollback / 100;
 
-console.log(fullPrice);
-console.log(Math.ceil(servicePercentPrice));
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+}
 
-switch (true) {
-  case fullPrice > 30000:
-    console.log("Даем скидку в 10%");
-    break;
-  case fullPrice > 15000 && fullPrice < 30000:
-    console.log("Даем скидку в 5%");
-    break;
-  case fullPrice < 15000 && fullPrice > 0:
-    console.log("Скидка не предусмотрена");
-    break;
-  case fullPrice <= 0:
-    console.log("Что-то пошло не так")
-};
+const getRollbackMessage = function (price) {
+  if (price >= 30000) {
+    return "Даем скидку в 10%"
+  } else if (price >= 15000 && price < 30000) {
+    return "Даем скидку в 5%"
+  } else if (price < 15000 && price > 0) {
+    return "Скидка не предусмотрена"
+  } else if (price <= 0) {
+    return "Что-то пошло не так"
+  }
+}
+
+showTypeOf(title)
+showTypeOf(screenPrice)
+showTypeOf(adaptive)
+
+console.log(getRollbackMessage(fullPrice));
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(servicePercentPrice);
+
+console.log("Стоимость вёрстки экранов " + screenPrice + " рублей/долларов/гривен/юаней");
